b3012577167e89fcfdf9b0a55124c261bcb03ce5
Luc Perkins <lucperkins@gmail.com>
2024-07-18 14:15:12 -0700

    Fix flake

--- flake.nix
+++ flake.nix
@@ -6,12 +6,12 @@
   outputs = { self, ... }@inputs:
     let
       forSystems = s: f: inputs.nixpkgs.lib.genAttrs s (system: f rec {
-        pkgs = import inputs.nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+        pkgs = import inputs.nixpkgs { inherit system; };
       });
       forEachSupportedSystem = forSystems [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
     in
     {
-      devShells = forEachSupportedSystem ({ system, pkgs }:
+      devShells = forEachSupportedSystem ({ pkgs }:
         {
           default = pkgs.mkShell {
             packages = with pkgs; [ ponysay ];
